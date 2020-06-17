344b5797ce12c5371c79e28041c3dad6f6f023ea
Cloud User <centos@ip-172-31-7-197.eu-west-2.compute.internal>
2020-06-17 11:01:06 +0000

    Ex5 Final

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,41 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Traffic Lights
-// Student Name:
-// Date: 
+// Student Name:Alex W
+// Date: 17/6/20
 //
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+	);
+
+	//Parameters
+	parameter CLK_PERIOD = 10;
+
+	reg clk;
+	wire red;
+	wire amber;
+	wire green;
+
+	//clock
+initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+initial begin
+	#200
+	$finish;
+	end
+
+traffic top (
+	.clk(clk),
+	.red(red),
+	.amber(amber),
+	.green(green)
+	);
+endmodule
