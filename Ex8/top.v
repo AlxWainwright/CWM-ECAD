b7d870dbb928d087f46c47a01ef4282b614331ed
Cloud User <centos@ip-172-31-7-197.eu-west-2.compute.internal>
2020-06-19 16:04:27 +0000

    Ex8 WIP,

--- Ex8/top.v
+++ Ex8/top.v
@@ -12,3 +12,63 @@
 //  outputs:
 //           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
+
+
+module timestable( clk,rst,a,b,read,result);
+
+input clk;
+input rst;
+input read;
+input [2:0] a;
+input [2:0] b;
+output ref [5:0] result;
+
+wire [31:0] s_axi_raddr={24'h0 ,b ,a ,2'b0};
+wire rsta_busy;
+wire rstb_busy;
+wire s_axi_arready,s_axi_rvalid;
+wire s_axi_read;
+wire [31:0] s_axi_rdata;
+
+assign s_axi_read = read&(rsta_busy==0)&(rstb_busy==0)&s_axi_arready;
+
+always @(posedge clk)
+begin
+	if (rst) 
+	  result<=#1 0;
+	else begin
+	  result<=#1 s_axi_rvalid ? s_axi_rdata[5:0] : result;
+	end
+end
+
+blk_mem_gen_0 timestable_mem (
+
+	.rsta_busy(rsta_busy),
+	.rstb_busy(rstb_busy),
+	.s_aclk(clk),
+	.s_aresern(1'b1),
+	.s_axi_awaddr(32'h0),
+	.s_axi_awvalid(32'h0),
+	.s_axi_awready(),
+	.s_axi_wdata(32'h0),
+	.s_axi_wstrb(4'h0),
+	.s_axi_wvalid(1'b0),
+	.s_axi_wready(),
+	.s_axi_bresp(),
+	.s_axi_bvalid(),
+	.s_axi_bready(1'b1),
+	.s_axi_araddr(s_axi_raddr),
+	.s_axi_arvalid(s_axi_read),
+	.s_axi_arready(s_axi_arready),
+	.s_axi_rdata(s_axi_rdata),
+	.s_axi_rresp(),
+	.s_axi_rvalid(s_axi_rvalid),
+	.s_axi_rready(1'b1)
+				);
+
+
+
+
+
+
+
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -6,4 +6,43 @@
 // Description: A testbench module to test Ex8 - Times Table using AXI4-Lite
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns /100ps
 
+module top_tb(
+);
+
+parameter CLK_PERIOD = 10;
+reg clk;
+reg [2:0] a;
+reg [2:0] b;
+wire [5:0] result;
+reg read;
+
+initial
+begin
+	clk = 1'b0;
+	forever
+	 #(CLK_PERIOD/2) clk=~2;
+end
+
+initial begin
+	a=0;
+	b=0;
+	read=0;
+	#100;
+	read=1;
+	forever begin
+		#(2*CLK_PERIOD)
+		$display("a=%d, b=%d, a*b=%d", a,b,result);
+		$finish;
+		end;
+
+timestable top(
+	.clk(clk),
+	.a(a),
+	.b(b),
+	.result(result[5:0]),
+	.read(read)
+	);
+
+endmodule;
